a9c2da70c1670dcc86cade076c0890ebfffeae67
Oluwafemi Oyedele <oluwafemioyedele908@gmail.com>
2021-11-08 03:36:30 -1100

    Initial commit

---
+++ Demo.R
@@ -0,0 +1,9 @@
+# This contains steps to configure my personal Access Token
+library(usethis)
+create_github_token() # This will create a github token; copy the token to your clipboard
+
+library(gitcreds)
+gitcreds_set() # Follow the instruction and then paste the github token there
+
+use_git() # Follow the steps
+use_github()  # This will create a github repository with your new project
---
+++ README.md
@@ -0,0 +1,2 @@
+# Personal Access Token Setup for Github in R
+This Repository contains step to follow in order to setup a personal access token for git in R.
