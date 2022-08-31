2c5d45c74ab78aff8012d3bcd6f7353f3c0bd8de
BB1464 <oluwafemioyedele908@gmail.com>
2022-08-31 11:13:46 +0100

    Add new line

--- Demo.R
+++ Demo.R
@@ -1,5 +1,12 @@
 # This contains steps to configure my personal Access Token
 library(usethis)
+
+use_git_config(user.name = "username", user.email = "User.email")
+
+
+git_sitrep()
+
+
 create_github_token() # This will create a github token; copy the token to your clipboard
 
 library(gitcreds)
